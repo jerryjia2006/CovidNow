94e148883ce2e3a0571fac58c08f7a09f25d26c9
jerryjia2006 <66880337+jerryjia2006@users.noreply.github.com>
2020-06-15 14:55:03 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+name = input("best country in the world: ")
+
+
+if (name.capitalize() == "China"):
+    print (name + " is the smartest")
+elif (name.capitalize() == "Canada"):
+    print (name + " is great")
+else:
+    print ("oops, wrong answer")
+    
+
+fruits = [ 'apple', 'orange', 'banana', 'peach']
+
+for fruit in fruits:
+    print(fruit)
